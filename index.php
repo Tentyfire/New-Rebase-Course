23a94e98c89f24a956428dcf38156828d9f86150
Jeremy Mayhew <jeremy.mayhew@oclock.io>
2021-03-22 10:23:54 +0100

    Add the reality on the question

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ echo "hello World !";
 function hello() {
     $hello = "hello";
     $world = "world";
-    return "hello world !";
+    return "hello world !" . "J'en ai marre de voir cette phrase...";
 }
